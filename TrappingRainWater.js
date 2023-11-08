6591a319d7c7c91d8ad231d4880e9536f190b161
David <doladepo128@gmail.com>
2023-11-08 23:59:56 +0100

    Trapping rain water

---
+++ TrappingRainWater.js
@@ -0,0 +1,10 @@
+function TrappingRainWater(height) {
+  let n = height.length();
+  let result = 0;
+  let maxLeft = height[0];
+  let maxRight = height[n - 1];
+  let left  = 1;
+  let right = n - 2;
+
+  while
+}
